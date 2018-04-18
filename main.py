9237cb0bd547f7095943b7b9cd192cfbd44a3b3e
Andrew McGhie <mcghiea@gmail.com>
2018-04-18 00:37:27 -0500

    add username/residence post foreign key crossreference, add secret key

--- main.py
+++ main.py
@@ -1,10 +1,59 @@
-from flask import Flask, request, redirect, render_template, url_for
+from flask import Flask, request, redirect, render_template, url_for, session
+from flask_sqlalchemy import SQLAlchemy 
 import os
 import cgi
 
-app = Flask(__name__)
 
-app.config['DEBUG'] = True 
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://placebook:asdf@localhost:8889/placebook'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+app.secret_key = 'y337kGcys&zP3B'
+
+class UserInfo(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120))
+    password = db.Column(db.String(120))
+    email = db.Column(db.String(120))
+    posts = db.relationship('ResidenceInfo', backref='owner')
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.password = password
+        self.email = email
+
+class ResidenceInfo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user_info.id'))
+    street = db.Column(db.String(120))
+    apt = db.Column(db.String(120))
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    zipcode = db.Column(db.Integer)
+    residence = db.Column(db.String(120))
+    room_number = db.Column(db.String(5))
+    building = db.Column(db.String(120))
+    amenities = db.Column(db.String(400))
+    management = db.Column(db.String(120))
+    rating = db.Column(db.Integer)
+    comment = db.Column(db.String(120))
+  
+    def __init__(self, owner, street, apt, city, state, zipcode, residence, room_number, building, amenities, management, rating, comment):
+        self.owner = owner
+        self.street = street
+        self.apt = apt
+        self.city = city
+        self.state = state
+        self.zipcode = zipcode
+        self.residence = residence
+        self.room_number = room_number
+        self.building = building
+        self.amenities = amenities
+        self.managment = management
+        self.rating = rating
+        self.comment = comment
 
 @app.route('/')
 def index():
@@ -19,6 +68,10 @@ def create():
         password = request.form['password']
         verify = request.form['verify']
         email = request.form['email']
+        
+        existing_user = UserInfo.query.filter_by(email=email).first()
+        duplicate_username = UserInfo.query.filter_by(username=username).first()
+        
         username_error = ""
         password_error = ""
         email_error = ""
@@ -29,6 +82,8 @@ def create():
         username_error = "Username cannot contain a space"
     elif len(username) > 20 or len(username) < 3:
         username_error = "User name must be betweeen 3 and 20 characters"
+    elif duplicate_username:
+        username_error = "This username has already been registered"
     
     if password == "":
         password_error = "Must enter a password"
@@ -47,11 +102,18 @@ def create():
         email_error = "Email address must be betweeen 3 and 20 characters"
     elif email != "" and (email.count("@") != 1 or email.count(".") != 1):
         email_error= "Invalid email address"
+    elif existing_user:
+        email_error = "This email address already has a registered user"    
     
     if username_error or password_error or email_error:
         return render_template('create.html', title = "User Signup", username=username, password="", verify="", email=email, username_error=username_error, password_error=password_error, email_error=email_error)
-    else:
+
+    else: 
+        new_user  = UserInfo(username, email, password)
+        db.session.add(new_user)
+        db.session.commit()
         return redirect('/welcome?username={0}'.format(username))
+    
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
@@ -60,7 +122,11 @@ def login():
     else:
         username = request.form['username']
         password = request.form['password']
-        verify = request.form['verify']
+    
+        
+        user_exists = UserInfo.query.filter_by(username=username).first() 
+        if user_exists:
+            print(user_exists.username)
         username_error = ""
         password_error = ""
     
@@ -70,6 +136,9 @@ def login():
         username_error = "Username cannot contain a space"
     elif len(username) > 20 or len(username) < 3:
         username_error = "User name must be betweeen 3 and 20 characters"
+    elif not user_exists:
+        username_error = "Username does not exist"
+
     
     if password == "":
         password_error = "Must enter a password"
@@ -77,11 +146,14 @@ def login():
         password_error = "Password cannot contain a space"
     elif len(password) > 20 or len(password) < 3:
         password_error = "Password must be betweeen 3 and 20 characters"
+    elif user_exists and user_exists.password != password:
+        password_error = "Incorrect password"
     
     if username_error or password_error:
         return render_template('login.html', title = "User Login", username=username, password="", verify="", username_error=username_error, password_error=password_error)
     else:
-        return redirect('/welcome?username={0}'.format(username))
+        session['username'] = username
+        return redirect('/add-residence')
 
 
 
@@ -95,6 +167,7 @@ def add():
     if request.method == 'GET':
         return render_template('add-residence.html', title="Add Residence")
     else:
+        owner = UserInfo.query.filter_by(username=session['username']).first()
         street = request.form['street']
         apt = request.form['apt']
         city = request.form['city']
@@ -103,6 +176,7 @@ def add():
         residence = request.form['residence']
         room_number = request.form['room-number']
         building = request.form['building']
+        amenities = request.form['amenities']
         management = request.form['management']
         rating = request.form['rating']
         comment = request.form['comment']
@@ -131,13 +205,13 @@ def add():
     elif len(state) > 2:
         state_error = "Enter valid state abbreviation" 
 
-    if residence == "Select":
+    if residence == "":
         residence_error = "Please select a residence type"
 
-    if room_number == "select":
+    if room_number == "":
         room_error = "Please select the number of rooms in the residence"
 
-    if rating == "Select":
+    if rating == "":
         rating_error = "Please select a rating"
 
     if comment == "":
@@ -145,11 +219,16 @@ def add():
    
     
     if street_error or apt_error or city_error or state_error or zip_error or residence_error or room_error or rating_error or comment_error:
-        return render_template('add-residence.html', title = "Add Residence", street=street, apt=apt, city=city, state=state, zipcode=zipcode, building=building, management=management, rating=rating, street_error=street_error, apt_error=apt_error, city_error=city_error, state_error=state_error, zip_error=zip_error, rating_error=rating_error)
+        return render_template('add-residence.html', title = "Add Residence", street=street, apt=apt, city=city, state=state, zipcode=zipcode, building=building, management=management, rating=rating, comment=comment, street_error=street_error, apt_error=apt_error, city_error=city_error, state_error=state_error, zip_error=zip_error, rating_error=rating_error)
     else:
+        new_residence = (ResidenceInfo(owner, street, apt, city, state, zipcode, residence, room_number, building, amenities, management, rating, comment))
+        db.session.add(new_residence)
+        db.session.commit()
         return redirect('/thankyou')    
 @app.route('/thankyou')
 def thankyou():
-    username = request.args.get('username')
+    username =  session['username']
     return render_template('thankyou.html', title = 'Thank You!!', username=username)
-app.run()
+
+if __name__ == '__main__':
+    app.run()
